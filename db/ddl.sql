6604a4c09eb672f5ec7dd352f369167b37a855f6
akagaeng <akagaeng@gmail.com>
2020-12-18 06:33:59 +0900

    feat: update database changes, add role

--- db/ddl.sql
+++ db/ddl.sql
@@ -10,16 +10,17 @@ CREATE TABLE `account`
     `id`         varchar(36) PRIMARY KEY,
     `email`      varchar(255) NOT NULL,
     `password`   varchar(100) NOT NULL,
-    `created_at` timestamp DEFAULT (datetime('now','localtime')),
-    `updated_at` timestamp DEFAULT (datetime('now','localtime'))
+    `role`       varchar(10)  NOT NULL,
+    `created_at` timestamp DEFAULT (datetime('now', 'localtime')),
+    `updated_at` timestamp DEFAULT (datetime('now', 'localtime'))
 );
 
 CREATE TABLE `passenger`
 (
     `id`         varchar(36) PRIMARY KEY,
     `account_id` varchar(36) NOT NULL,
-    `created_at` timestamp DEFAULT (datetime('now','localtime')),
-    `updated_at` timestamp DEFAULT (datetime('now','localtime')),
+    `created_at` timestamp DEFAULT (datetime('now', 'localtime')),
+    `updated_at` timestamp DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
 );
 
@@ -27,8 +28,8 @@ CREATE TABLE `driver`
 (
     `id`         varchar(36) PRIMARY KEY,
     `account_id` varchar(36) NOT NULL,
-    `created_at` timestamp DEFAULT (datetime('now','localtime')),
-    `updated_at` timestamp DEFAULT (datetime('now','localtime')),
+    `created_at` timestamp DEFAULT (datetime('now', 'localtime')),
+    `updated_at` timestamp DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
 
 );
@@ -41,10 +42,10 @@ CREATE TABLE `dispatch`
     `driver_id`          varchar(36),
     `driver_location`    varchar(100),
     `status`             varchar(10) DEFAULT 'waiting', -- waiting | finished
-    `requested_at`       timestamp   DEFAULT (datetime('now','localtime')),
+    `requested_at`       timestamp   DEFAULT (datetime('now', 'localtime')),
     `finished_at`        timestamp,
-    `created_at`         timestamp   DEFAULT (datetime('now','localtime')),
-    `updated_at`         timestamp   DEFAULT (datetime('now','localtime')),
+    `created_at`         timestamp   DEFAULT (datetime('now', 'localtime')),
+    `updated_at`         timestamp   DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (`passenger_id`) REFERENCES `passenger` (`id`),
     FOREIGN KEY (`driver_id`) REFERENCES `driver` (`id`)
 );
